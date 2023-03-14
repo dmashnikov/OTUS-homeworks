1269e79b7931bc6f4c0f6ea127379609b4f78f08
Dmitrii <dmitrymashnikov@gmail.com>
2023-03-14 17:52:34 +0300

    add second test and increase solve method

--- src/main/java/org/dmashnikov/MathUtils.java
+++ src/main/java/org/dmashnikov/MathUtils.java
@@ -1,10 +1,21 @@
 package org.dmashnikov;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MathUtils {
-    public double[] solve(double a, double b, double c) {
+    private static final double EPSILON = 0.000005;
+
+    public List<Double> solve(double a, double b, double c) {
+        List<Double> solutions = new ArrayList<>();
         double D = b * b - 4 * a * c;
         if (D < 0) {
-            return new double[0];
+            return solutions;
+        }
+        if (Math.abs(D) < EPSILON) {
+            double x = - b / (2 * a);
+            solutions.add(x);
+            return solutions;
         }
         return null;
     }
--- src/test/java/org/dmashnikov/MainTests.java
+++ src/test/java/org/dmashnikov/MainTests.java
@@ -1,19 +1,34 @@
 package org.dmashnikov;
 
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
 
 public class MainTests {
 
     @Test
-    void test1() {
+    void shouldReturnEmptyArr_whenDiscriminantLessThanZero() {
         double a = 2;
         double b = 3;
         double c = 7;
 
         MathUtils mathUtils = new MathUtils();
-        double [] actual = mathUtils.solve(a, b, c);
-        Assertions.assertEquals(0, actual.length);
+        List<Double> actual = mathUtils.solve(a, b, c);
+        assertThat(actual).isEmpty();
+    }
 
+    @Test
+    void shouldReturnOneValue_whenDiscriminantEqZero() {
+        double a = 9;
+        double b = 12;
+        double c = 4;
+        double expected = -0.666666;
+
+        MathUtils mathUtils = new MathUtils();
+        List<Double> actual = mathUtils.solve(a, b, c);
+        assertThat(actual.size()).isEqualTo(1);
+        assertThat(actual.get(0)).isEqualTo(expected, Offset.offset(0.000005));
     }
 }
